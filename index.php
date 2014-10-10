a525256756a8882a4b5b1ba87927478efff614b2
jgadsbypeet <jgadsbypeet@gmail.com>
2014-10-10 06:03:24 +0100

    CRUK fontify

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <title>Look up Tool Demo</title>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link href="style.css" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 </head>
